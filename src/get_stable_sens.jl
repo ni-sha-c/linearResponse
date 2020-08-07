78ebf1a04b7602f52a818ab13598532d6a7ac8e3
Nisha Chandramoorthy <nishac@mit.edu>
2020-08-07 10:30:50 -0400

    compute stable sen in parallel

--- src/get_stable_sens.jl
+++ src/get_stable_sens.jl
@@ -1,51 +1,50 @@
 include("../examples/baker.jl")
 using LinearAlgebra
 using JLD
-function stable_sens(s4)
-d = 2
-p = 4
-
-nSteps = 10
-u_trj = zeros(d, nSteps)
-du_trj = zeros(d,d,nSteps)
-s = zeros(p)
-
-
-vs = zeros(2)
-q = rand(2)
-q /= norm(q)
-nm_q = zeros(nSteps)
-le = 0.
-# J = cos(4y)
-
-n_exps = size(s4)[1]
-dJds = zeros(n_exps)
-n_rep = 10
-for k=1:n_exps
-		s[4] = s4[k]
-		
-	for j=1:n_rep
-		u = 2*pi*rand(d)
-		u_trj .= step(u, s, nSteps-1)
-		du_trj .= dstep(u_trj, s)
-		x = pert(u_trj, 4)
-
-		for i = 1:nSteps
-			vs .= du_trj[:,:,i]*vs + x[:,i]
-			q .= du_trj[:,:,i]*q 
-			
-			nm_q[i] = norm(q)
-			le += log(nm_q[i])/nSteps
-			q ./= nm_q[i]
-
-			vs .-= dot(vs,q)*q
-			x1, x2 = u_trj[1,i], u_trj[2,i]
-			dJdu = [0., -4*sin(4*x2)]
-			dJds[k] += dot(dJdu, vs)/nSteps/n_rep
-		end
-		#@show le, vs, dJds[k], q
+function stable_sens(s,nSteps)
+	vs = zeros(2)
+	q = rand(2)
+	q /= norm(q)
+	le = 0.
+	u = 2*pi*rand(d)
+	u_trj = step(u, s, nSteps-1)
+	du_trj = dstep(u_trj, s)
+	x = pert(u_trj, 4)
+	dJds = 0.
+	for i = 1:nSteps
+		vs .= du_trj[:,:,i]*vs + x[:,i]
+		q .= du_trj[:,:,i]*q 
+		nm_q = norm(q)
+		le += log(nm_q)/nSteps
+		q ./= nm_q
+		vs .-= dot(vs,q)*q
+		x2 = u_trj[2,i]
+		dJdu = [0., -4*sin(4*x2)]
+		dJds += dot(dJdu, vs)/nSteps
 	end
+	#@show le, dJds
+	return dJds
 end
-return dJds
+function get_stable_sens(s4)
+	d = 2
+	p = 4
+	nSteps = 100
+	s = zeros(p)
+	# J = cos(4y)
+	n_exps = size(s4)[1]
+	dJds = zeros(n_exps)
+	n_rep = 160000
+	dJds_proc = SharedArray{Float64}(n_exps)
+	for k=1:n_exps
+		s[4] = s4[k]
+		dJds_proc .= 0.
+		t = @distributed for j=1:n_rep
+			dJds_proc[j] = stable_sens(s,nSteps)/n_rep
+		end
+		wait(t)
+		dJds[k] = sum(dJds_proc)
+		@show dJds[k]
+	end
+	return dJds
 end
 	
