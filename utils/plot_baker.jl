7f9098a94b82779448439500581582339933a5c3
Nisha Chandramoorthy <nishac92@gmail.com>
2021-03-05 08:56:43 -0500

    Keep only the more general function that plots up to 40 rectangles

--- utils/plot_baker.jl
+++ utils/plot_baker.jl
@@ -167,83 +167,15 @@ function plot_mapping_vs_params()
 		
 	end
 end
-function plot_annotated_map_original(s)
-	N = 500000
-	u1 = [[π*rand(), 2π*rand()] for i=1:N]
-	u2 = [[π*rand() + π, 2π*rand()] for i=1:N]
-	u1_next = next.(u1, Ref(s))
-	u1_next = hcat(u1_next...)
-	u1 = hcat(u1...)
-	u2_next = next.(u2, Ref(s))
-	u2_next = hcat(u2_next...)
-	u2 = hcat(u2...)
-
-
-	fig = figure(figsize=(8,6))
-	ax = fig.add_subplot(111)
-
-	fig1 = figure(figsize=(8,6))
-	ax1 = fig1.add_subplot(111)
-	ax.set_xlim([0.,2π])
-	ax.set_ylim([0.,2π])
-	ax1.set_xlim([0.,2π])
-	ax1.set_ylim([0.,2π])
-	
-	ax1.plot(u1_next[1,:], u1_next[2,:],".",ms=1.)
-	ax1.plot(u2_next[1,:], u2_next[2,:],".",ms=1.)
-	ax.plot(u1[1,:], u1[2,:],".")
-	ax.plot(u2[1,:], u2[2,:],".")
-
-	ax.xaxis.set_tick_params(labelsize=30)
-	ax1.xaxis.set_tick_params(labelsize=30)
-	ax.yaxis.set_tick_params(labelsize=30)
-	ax1.yaxis.set_tick_params(labelsize=30)
-
-	ax.axis("scaled")
-	ax1.axis("scaled")
-
-end
-function plot_annotated_map_vertical_strips(s)
-	N = 500000
-	n_gr = 20
-	fig = figure(figsize=(8,6))
-	ax = fig.add_subplot(111)
-
-	fig1 = figure(figsize=(8,6))
-	ax1 = fig1.add_subplot(111)
-	ax.set_xlim([0.,2π])
-	ax.set_ylim([0.,2π])
-	ax1.set_xlim([0.,2π])
-	ax1.set_ylim([0.,2π])
-
-	for j = 1:n_gr
-		u1 = [[2π/n_gr*rand() + (j-1)*2π/n_gr, 2π*rand()] for i=1:N]
-		u1_next = next.(u1, Ref(s))
-		u1_next = hcat(u1_next...)
-		u1 = hcat(u1...)
-
-	
-		ax1.plot(u1_next[1,:], u1_next[2,:],".",ms=1.)
-		ax.plot(u1[1,:], u1[2,:],".")
-	end
-	ax.xaxis.set_tick_params(labelsize=30)
-	ax1.xaxis.set_tick_params(labelsize=30)
-	ax.yaxis.set_tick_params(labelsize=30)
-	ax1.yaxis.set_tick_params(labelsize=30)
-
-	ax.axis("scaled")
-	ax1.axis("scaled")
-
-end
 function plot_annotated_map_rectangles(s)
 	N = 500000
 	n_gr_x = 2
-	n_gr_y = 2
+	n_gr_y = 8
 	dx = 2π/n_gr_x
 	dy = 2π/n_gr_y
 	fig = figure(figsize=(8,6))
 	ax = fig.add_subplot(111)
-
+	clrs = matplotlib.cm["tab20c"]
 	fig1 = figure(figsize=(8,6))
 	ax1 = fig1.add_subplot(111)
 	ax.set_xlim([0.,2π])
@@ -260,9 +192,12 @@ function plot_annotated_map_rectangles(s)
 			u1_next = hcat(u1_next...)
 			u1 = hcat(u1...)
 
-	
-			ax1.plot(u1_next[1,:], u1_next[2,:],".",ms=1.)
-			ax.plot(u1[1,:], u1[2,:],".")
+			ind = k + (j-1)*n_gr_y
+			if (ind > 20) 
+					ind = mod(ind, 20)
+			end
+			ax1.plot(u1_next[1,:], u1_next[2,:],".",color=clrs.colors[ind],ms=1.)
+			ax.plot(u1[1,:], u1[2,:],".", color=clrs.colors[ind], ms=1.0)
 		end
 	end
 	ax.xaxis.set_tick_params(labelsize=30)
