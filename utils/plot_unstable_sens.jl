8f9a4c1b5c3c3fe36358b855798db89f4d9d6c2c
Nisha Chandramoorthy <nishac@mit.edu>
2021-03-07 12:34:39 -0500

    change fontsize

--- utils/plot_unstable_sens.jl
+++ utils/plot_unstable_sens.jl
@@ -5,11 +5,11 @@ function plot_unstable_sens()
 	s1_arr = X["s1"]
 	J_arr = X["J"]
 	fig, ax = subplots(1,1)
-	ax.plot(s1_arr, J_arr, "x", ms=4.0)
-    ax.xaxis.set_tick_params(labelsize=28)
-    ax.yaxis.set_tick_params(labelsize=28)
-    ax.set_xlabel(L"$s_1$",fontsize=28)
-    ax.set_ylabel(L"$\langle J\rangle$",fontsize=28)
+	ax.plot(s1_arr, J_arr, ".", ms=10.0)
+    ax.xaxis.set_tick_params(labelsize=32)
+    ax.yaxis.set_tick_params(labelsize=32)
+    ax.set_xlabel(L"$s_1$",fontsize=32)
+    ax.set_ylabel(L"$\langle J\rangle$",fontsize=32)
 	ax.grid(true)
 	
 	X = load("../data/unstable_sens/dJds1_test.jld")
@@ -19,15 +19,16 @@ function plot_unstable_sens()
 	X = load("../data/obj_erg_avg/cos4y_s1_sens.jld")
 	J = X["J"]
 
-	eps = 1.e-2
+	eps = 2e-2
 	n = size(dJds)[1]
 	J_pts = reshape([J .- eps*dJds J .+ eps*dJds], n, 
 					2)'
 	s_pts = reshape([s1 .- eps s1 .+ eps], n, 
 					2)'
 
-	ax.plot(s_pts, J_pts, "g",lw=2.0)
-
+	ax.plot(s_pts[:,1:2:end-1], J_pts[:,1:2:end-1], "k",lw=6.0)
+	ax.plot(s_pts[:,end], J_pts[:,end],"k",lw=6.0,label=L"$d_s\langle J\rangle$ from S3")
+	ax.legend(fontsize=32)
 	return dJds
 
 end
