e61b307e9d2d33c11ad936fe6443e9c87edcf1bf
Nisha Chandramoorthy <nishac@mit.edu>
2021-03-22 16:54:19 -0400

    change z to alpha

--- src/get_sens_lorenz.jl
+++ src/get_sens_lorenz.jl
@@ -3,11 +3,12 @@ using LinearAlgebra
 using JLD
 using SharedArrays
 using Distributed
-#function sens(s,nSteps)
+function sens(s,nSteps)
     d = 3
     u = rand(d)
     u_trj = step(u, s, nSteps)
     x, y, z = view(u_trj,1,:), view(u_trj,2,:), view(u_trj,3,:)
+   
     du_trj = dstep(u_trj, s)
     ddu_trj = d2step(u_trj,s)
     vs = zeros(d)
@@ -26,12 +27,10 @@ using Distributed
     g = zeros(nSteps)
     J = copy(z)
    
-	#=
     # Df represents the derivative of f 
     # on the 
     # unstable manifold.
     # Xu = a q
-    # d = 2
     Xu = zeros(d, nSteps)
     a = zeros(nSteps)
     Da = zeros(nSteps)
@@ -41,47 +40,42 @@ using Distributed
     rho = s[2]
     # nSteps large number.
     for i = 1:nSteps-1
-    	dui = du_trj[:,:,i] # for large systems, can't store Jacobian.
+	@show i
+	dui = du_trj[:,:,i] # for large systems, can't store Jacobian.
     	ppi = pp[:,i]
 	dppi = dpp[:,:,i]
-	xi, yi = x[i], y[i], z[i]
+	xi, yi, zi = x[i], y[i], z[i]
 
     	q1 .= dui*q
-    	z = norm(q1)
-    	q1 ./= z
-
-    	z2 = z*z
+    	alpha = norm(q1)
+    	q1 ./= alpha
+    	alpha2 = alpha*alpha
     	
     	vs1 .= dui*vs + ppi
     	a[i+1] = dot(vs1, q1)
     	vs1 .-= a[i+1]*q1
-
     	
     	d2q = reshape([dot(ddu_trj[:,j,i],
-    				q) for j=1:4],2,2)
-    	Dq = d2q*q/z2 + dui*Dq/z2
-    	dzdx = dot(z2*Dq, q1)
+    				q) for j=1:d*d],d,d)
+    	Dq = d2q*q/alpha2 + dui*Dq/alpha2
+    	dalphadx = dot(alpha2*Dq, q1)
     	Dq .= Dq .- dot(Dq,q1)*q1
-    	
 
-
-    	Dvs1 .= d2q*vs/z + dui*Dvs/z + dppi*q1/z
+    	Dvs1 .= d2q*vs/alpha + dui*Dvs/alpha + dppi*q1/alpha
     	Da[i+1] = dot(vs1, Dq) + dot(Dvs1, q1)
     	Dvs1 .= Dvs1 - Da[i+1]*q1 - a[i+1]*Dq
     	Delta_Da = dot(Dvs1, q1) + dot(vs1, Dq) 
     	Dvs1 .-= Delta_Da*q
     	Da[i+1] += Delta_Da
     	
-    	g[i+1] = g[i]/z - dzdx/z2 
-    	dJdu = [0., -4*sin(4*yi)]
+    	g[i+1] = g[i]/alpha - dalphadx/alpha2 
+    	dJdu = [0., 0., 1.]
     	dJds_st += dot(dJdu, vs)/nSteps
-
     	vs .= vs1
     	q .= q1
     	Dvs .= Dvs1
-
-
     end
+
     for n = 1:N
     	J_shift = J[n+1:end]
     	nJ = length(J_shift)
@@ -91,12 +85,12 @@ using Distributed
     	dJds_ust -= dot(J_shift,Da_shift)/nJ 
     	dJds_ust -= dot(J_shift,g_shift.*a_shift)/nJ
     end
-    =#
+   
     @show dJds_st
     return dJds_st + dJds_ust
-#end
+end
 function get_sens(s)
-    nSteps = 500000
+    nSteps = 50
     n_exps = size(s)[2]
     dJds = zeros(n_exps)
     n_rep = 16
