12cdcf7747a15712d36ebcb931af180dc6701b8a
Nisha Chandramoorthy <nishac@mit.edu>
2021-03-22 14:12:21 -0400

    fix dpert etc

--- examples/lorenz.jl
+++ examples/lorenz.jl
@@ -1,29 +1,28 @@
 dt = 0.005
 function step(x0, s, n)
-	x_trj = zeros(3, n+1)
-	x_trj[:,1] = x0
-	sigma, rho, beta = s
-    d, m = size(u0)
+    x_trj = zeros(3, n+1)
+    x_trj[:,1] = x0
+    sigma, rho, beta = s
     n = n+1
     for i = 2:n
-    	x = x_trj[1,i-1]
-    	y = x_trj[2,i-1]
-    	z = x_trj[3,i-1]
+        x = x_trj[1,i-1]
+        y = x_trj[2,i-1]
+        z = x_trj[3,i-1]
     
-    	x_trj[1,i] = x + dt*(sigma*(y - x))
-    	x_trj[2,i] = y + dt*(x*(rho - z) - y)
-    	x_trj[3,i] = z + dt*(x*y - beta*z)
+        x_trj[1,i] = x + dt*(sigma*(y - x))
+        x_trj[2,i] = y + dt*(x*(rho - z) - y)
+        x_trj[3,i] = z + dt*(x*y - beta*z)
     end 
     return x_trj
 end
 function next(u, s)
-	x, y, z = u[1], u[2], u[3]
-	u_next = similar(u)
-	sigma, rho, beta = s
-	u_next[1] = x + dt*(sigma*(y - x))
+    x, y, z = u[1], u[2], u[3]
+    u_next = similar(u)
+    sigma, rho, beta = s
+    u_next[1] = x + dt*(sigma*(y - x))
     u_next[2] = y + dt*(x*(rho - z) - y)
     u_next[3] = z + dt*(x*y - beta*z)
-	return u_next
+    return u_next
 end
 function dstep(u::Array{Float64,2}, s::Array{Float64,1})
     sigma, rho, beta = s
@@ -41,12 +40,12 @@ function dstep(u::Array{Float64,2}, s::Array{Float64,1})
     @. du[:,3,2] = dt*x
     @. du[:,3,3] = 1.0 - dt*beta
     return reshape([du[:,:,1]'; du[:,:,2]'; 
-    				du[:,:,3]'], d, d, n)
+        			du[:,:,3]'], d, d, n)
 end
 function dstep(u::Array{Float64,1},s::Array{Float64,1})
-	du = zeros(3,3)
-	x, y, z = u[1], u[2], u[3]
-	sigma, rho, beta = s
+    du = zeros(3,3)
+    x, y, z = u[1], u[2], u[3]
+    sigma, rho, beta = s
     @. du[1,1] = 1.0 - dt*sigma
     @. du[1,2] = dt*sigma
     @. du[2,1] = dt*(rho - z) 
@@ -55,68 +54,75 @@ function dstep(u::Array{Float64,1},s::Array{Float64,1})
     @. du[3,1] = dt*y
     @. du[3,2] = dt*x
     @. du[3,3] = 1.0 - dt*beta
-	return du
+    return du
 end
 function d2step(u::Array{Float64,2}, s::Array{Float64,1})
-	n = size(u)[2]
-	ddu = zeros(3,9,n)
-	for i = 1:n
-		x, y, z = u[1,i], u[2,i], u[3,i]
-		ddu[:,2,i] = [0., 0., -dt]
-		ddu[:,3,i] = [0., dt, 0.]
-		ddu[:,6,i] = [dt, 0., 0.]
-		ddu[:,8,i] = [-dt, 0., 0.]
-	end
-	return ddu
+    n = size(u)[2]
+    ddu = zeros(3,9,n)
+    for i = 1:n
+    	x, y, z = u[1,i], u[2,i], u[3,i]
+    	ddu[:,2,i] = [0., 0., -dt]
+    	ddu[:,3,i] = [0., dt, 0.]
+    	ddu[:,6,i] = [dt, 0., 0.]
+    	ddu[:,8,i] = [-dt, 0., 0.]
+    end
+    return ddu
 end
 function d2step(u::Array{Float64,1}, s::Array{Float64,1})
-	ddu = zeros(3,9)
-	x, y, z = u[1], u[2], u[3]
-	ddu[:,2] = [0., 0., -dt]
-	ddu[:,3] = [0., dt, 0.]
-	ddu[:,6] = [dt, 0., 0.]
-	ddu[:,8] = [-dt, 0., 0.]
-	return ddu
+    ddu = zeros(3,9)
+    x, y, z = u[1], u[2], u[3]
+    ddu[:,2] = [0., 0., -dt]
+    ddu[:,3] = [0., dt, 0.]
+    ddu[:,6] = [dt, 0., 0.]
+    ddu[:,8] = [-dt, 0., 0.]
+    return ddu
 end
 function vectorField(u,s)
     d, n = size(u)
     sigma, rho, beta = s
-	x, y, z = view(u,1,:), view(u,2,:), view(u,3,:)
+    x, y, z = view(u,1,:), view(u,2,:), view(u,3,:)
     return [sigma.*(y - x)  x.*(rho .- z) - y  x.*y - beta.*z]'
 end
 function pert(u::Array{Float64,2},s::Array{Float64,1})
-	n = size(u)[2]
-	x = view(u,1,:)
-	pp = zeros(3, n)
-	@. pp[2,:] = dt*x
-	return pp
+    n = size(u)[2]
+    x = view(u,1,:)
+    pp = zeros(3, n)
+    @. pp[2,:] = dt*x
+    return pp
 end
 function pert(u::Array{Float64,1},s::Array{Float64,1})
-	return [0, dt*u[1], 0]
+    return [0, dt*u[1], 0]
 end
 function dpert(u::Array{Float64,1},s::Array{Float64,1})
-	x = u[1]
-	dpp = zeros(3,3)
-	dpp[2,1] = dt
-	return dpp
+    x = u[1]
+    dpp = zeros(3,3)
+    dpp[2,1] = dt
+    return dpp
 end
+function dpert(u::Array{Float64,2},s::Array{Float64,1})
+    x = u[1,:]
+    dpp = zeros(3,3,:)
+    dpp[2,1,:] = dt
+    return dpp
+end
+
 ### AD functions
 function lorenz63_rhs_ad(du, u, s, t)
     du[1] = s[1]*(u[2] - u[1])
-	du[2] = u[1]*(s[2] - u[3]) - u[2]
-	du[3] = u[1]*u[2] - s[3]*u[3]
+    du[2] = u[1]*(s[2] - u[3]) - u[2]
+    du[3] = u[1]*u[2] - s[3]*u[3]
 end
 function lorenz63_ad(u0, s, n)
-	t = n*dt
+    t = n*dt
     prob = ODEProblem(lorenz63_rhs_ad, u0, (0.,t), s)
-	sol = Array(solve(prob, Tsit5(), saveat=dt))
-	return sol[:,end]
+    sol = Array(solve(prob, Tsit5(), saveat=dt))
+    return sol[:,end]
 end
 function obj_fun(u0, s)
     prob = ODEProblem(lorenz63_rhs_ad, u0, (0.,1.1), s)
-	#_prob = remake(prob,u0=u0,p=s) 
-	sol = solve(prob, Tsit5(), saveat=0.005)
-	sum(sol[3,:])/size(sol,2)
+    #_prob = remake(prob,u0=u0,p=s) 
+    sol = solve(prob, Tsit5(), saveat=0.005)
+    sum(sol[3,:])/size(sol,2)
 end
 
 
