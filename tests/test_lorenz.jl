bfc39f71b1cfadb677035bee6926fa5dfafb808a
Nisha Chandramoorthy <nishac@mit.edu>
2021-03-30 15:46:51 -0400

    incomplete dpert next array version tested. passes.

--- tests/test_lorenz.jl
+++ tests/test_lorenz.jl
@@ -153,7 +153,7 @@ function test_pert_arr()
 	@show pp_fd, pp
 	@test pp_fd ≈ pp atol=1.e-8
 end
-function test_dpert()
+function test_dpert_next()
 	eps = 1.e-6
 	s = [10., 28., 8.0/3]
 	u = rand(3)
@@ -167,7 +167,7 @@ function test_dpert()
 	dpert_dx_fd = (pert(u1px,s) - pert(u1mx,s))/(2*eps) 
 	dpert_dy_fd = (pert(u1py,s) - pert(u1my,s))/(2*eps) 
 	dpert_dz_fd = (pert(u1pz,s) - pert(u1mz,s))/(2*eps)
-	dpp = dpert(u, s)
+	dpp = dpert_next(u, s)
 
 	@show dpp[:,1] .- dpert_dx_fd
 	@show dpp[:,2] .- dpert_dy_fd
@@ -176,6 +176,28 @@ function test_dpert()
 	@test dpp[:,1] ≈ dpert_dx_fd atol=1.e-8
 	@test dpp[:,2] ≈ dpert_dy_fd atol=1.e-8
 	@test dpp[:,3] ≈ dpert_dz_fd atol=1.e-8
+end
+function test_dpert_next_arr()
+	eps = 1.e-6
+	s = [10., 28., 8.0/3]
+	u = rand(3,1)
+	u1px = u[:,1] .+ eps*[1,0,0]
+	u1mx = u[:,1] .- eps*[1,0,0]
+	u1py = u[:,1] .+ eps*[0,1,0]
+	u1my = u[:,1] .- eps*[0,1,0]
+	u1pz = u[:,1] .+ eps*[0,0,1]
+	u1mz = u[:,1] .- eps*[0,0,1]
 
+	dpert_dx_fd = (pert(u1px,s) - pert(u1mx,s))/(2*eps) 
+	dpert_dy_fd = (pert(u1py,s) - pert(u1my,s))/(2*eps) 
+	dpert_dz_fd = (pert(u1pz,s) - pert(u1mz,s))/(2*eps)
+	dpp = dpert_next(u, s)
 
+	@show dpp[:,1,1] .- dpert_dx_fd
+	@show dpp[:,2,1] .- dpert_dy_fd
+	@show dpp[:,3,1] .- dpert_dz_fd
+
+	@test dpp[:,1,1] ≈ dpert_dx_fd atol=1.e-8
+	@test dpp[:,2,1] ≈ dpert_dy_fd atol=1.e-8
+	@test dpp[:,3,1] ≈ dpert_dz_fd atol=1.e-8
 end
