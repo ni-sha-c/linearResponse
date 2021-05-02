5f26b29a1b086029b336dc7f815b9dace26f5649
Nisha Chandramoorthy <nishac@mit.edu>
2021-05-02 15:53:39 -0400

    Add plots of sensitivities superimposed on obj vs param plot

--- utils/plot_sens_lorenz.jl
+++ utils/plot_sens_lorenz.jl
@@ -19,40 +19,40 @@ function plot_errorbar()
 end
 function plot_lines()
 	X = load("../data/sens/lorenz/dJds_squareObj.jld")
-	rho_arr = X["s"]
-	dJds_arr = X["dJds"]
-	err_arr = sqrt.(X["var_dJds"])/4
-	@show err_arr
-	X = load("../data/obj_erg_avg/cos4y_s2.jld")
-    s2_arr = X["s2"]
-    J_arr = X["J"]
+	rho = X["rho"]
+	dJds = X["dJds"]
+	err = sqrt.(X["var_dJds"])/4
+	@show err
+	X = load("../data/obj_erg_avg/lorenz/zm28sq.jld")
+    s2 = X["s2"]
+    J = X["J"]
     fig, ax = subplots(1,1)
-    ax.plot(s2_arr, J_arr, ".", ms=10.0)
-    ax.xaxis.set_tick_params(labelsize=32)
-    ax.yaxis.set_tick_params(labelsize=32)
-    ax.set_xlabel(L"$s_2$",fontsize=32)
-    ax.set_ylabel(L"$\langle J\rangle$",fontsize=32)
+    ax.plot(s2, J, ".", ms=10.0)
+    ax.xaxis.set_tick_params(labelsize=28)
+    ax.yaxis.set_tick_params(labelsize=28)
+    ax.set_xlabel(L"$s$",fontsize=28)
+    ax.set_ylabel(L"$\langle J\rangle$",fontsize=28)
     ax.grid(true)
 
-    X = load("../data/sens/dJds_s2.jld")
+    eps = 5.e-2
 
-    dJds = X["dJds"]
-    s2 = X["s2"]
+	X = load("../data/obj_erg_avg/lorenz/zm28sq_dJds.jld")
+	J = X["J"]
+	s2 = X["s2"]
 
-    X = load("../data/obj_erg_avg/cos4y_s2_sens.jld")
-    J = X["J"]
-
-    eps = 2.e-2
-    n = size(dJds)[1]
-    J_pts = reshape([J .- eps*dJds J .+ eps*dJds], n,
+	s2 = s2[abs.(dJds) .<= 50]
+	J = J[abs.(dJds) .<= 50]
+    dJds = dJds[abs.(dJds) .<= 50]
+	n = size(dJds)[1]
+	J_pts = reshape([J .- eps*dJds J .+ eps*dJds], n,
                     2)'
     s_pts = reshape([s2 .- eps s2 .+ eps], n,
                     2)'
 
-    ax.plot(s_pts[:,1:n-1], J_pts[:,1:n-1], "k",lw=6.0)
-    ax.plot(s_pts[:,n], J_pts[:,n], "k",lw=6.0,label=L"$d_s\langle J\rangle$ from S3")
+    ax.plot(s_pts[:,1:n-1], J_pts[:,1:n-1], "k",lw=3.0)
+    ax.plot(s_pts[:,n], J_pts[:,n], "k",lw=3.0,label=L"$d_s\langle J\rangle$ from S3")
 
-    ax.legend(fontsize=32)
+    ax.legend(fontsize=28)
 
 end
 
