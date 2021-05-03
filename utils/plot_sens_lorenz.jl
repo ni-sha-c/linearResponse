4e75bfe9fb67034698e16dc19e2c5a429ccef3a2
Nisha Chandramoorthy <nishac@mit.edu>
2021-05-03 08:29:23 -0400

    finalize sens plots

--- utils/plot_sens_lorenz.jl
+++ utils/plot_sens_lorenz.jl
@@ -6,6 +6,11 @@ function plot_errorbar()
 	dJds_arr = X["dJds"]
 	err_arr = sqrt.(X["var_dJds"])/4
 	@show err_arr
+	sel = err_arr .<= 0.2
+	rho_arr = rho_arr[sel]
+	err_arr = err_arr[sel]
+	dJds_arr = dJds_arr[sel]
+
 	fig, ax = subplots(1,1)
 	ax.plot(rho_arr, dJds_arr, "X", ms=10.0)
 	ax.errorbar(rho_arr, dJds_arr, yerr=err_arr, linewidth=3.0, color="blue",fmt="none")
@@ -23,31 +28,33 @@ function plot_lines()
 	dJds = X["dJds"]
 	err = sqrt.(X["var_dJds"])/4
 
-	dJds = dJds[rho <= 31]
-	err = err[rho <= 31]
-
-
 	@show err
 	X = load("../data/obj_erg_avg/lorenz/zm28sq.jld")
     s2 = X["s2"]
     J = X["J"]
     fig, ax = subplots(1,1)
-    ax.plot(s2, J, ".", ms=20.0)
+    ax.plot(s2, J, ".", ms=25.0)
     ax.xaxis.set_tick_params(labelsize=28)
     ax.yaxis.set_tick_params(labelsize=28)
     ax.set_xlabel(L"$s$",fontsize=28)
     ax.set_ylabel(L"$\langle J\rangle$",fontsize=28)
     ax.grid(true)
 
-    eps = 1.2e-1
+    eps = 2e-1
 
 	X = load("../data/obj_erg_avg/lorenz/zm28sq_dJds.jld")
 	J = X["J"]
 	s2 = X["s2"]
+	
+	sel = rho .<= 30.5
 
-	J = J[rho <= 31]
-	s2 = s2[rho <= 31]
-	rho = rho[rho <= 31]
+	dJds = dJds[sel]
+	err = err[sel]
+
+
+	J = J[sel]
+	s2 = s2[sel]
+	rho = rho[sel]
 	s2 = s2[err .<= 1.8]
 	J = J[err .<= 1.8]
     dJds = dJds[err .<= 1.8]
@@ -58,11 +65,11 @@ function plot_lines()
                     2)'
 
     ax.plot(s_pts[:,1:n-1], J_pts[:,1:n-1], "k",lw=3.0)
-    ax.plot(s_pts[:,n], J_pts[:,n], "k",lw=3.0,label=L"$d_s\langle J\rangle$ from S3")
+    ax.plot(s_pts[:,n], J_pts[:,n], "k",lw=4.0,label=L"$d_s\langle J\rangle$ from S3")
 
     ax.legend(fontsize=28)
 
-	X = load("../data/sens/lorenz/dvarz_ds2_800.jld")
+	X = load("../data/sens/lorenz/dvarz_ds2_1500.jld")
 	rho = X["rho"]
 	dJds = X["dJds"]
 	err = sqrt.(X["var_dJds"])/4
@@ -71,6 +78,12 @@ function plot_lines()
 	J = X["J"]
 	s2 = X["s2"]
 
+	sel = .!(30 .>= rho .>= 29)	
+	dJds = dJds[sel]
+	err = err[sel]
+	s2 = s2[sel]
+	J = J[sel]
+
 	s2 = s2[err .<= 1.8]
 	J = J[err .<= 1.8]
     dJds = dJds[err .<= 1.8]
@@ -81,7 +94,7 @@ function plot_lines()
                     2)'
 
     ax.plot(s_pts[:,1:n-1], J_pts[:,1:n-1], "k",lw=3.0)
-    ax.plot(s_pts[:,n], J_pts[:,n], "k",lw=3.0,label=L"$d_s\langle J\rangle$ from S3")
+    ax.plot(s_pts[:,n], J_pts[:,n], "k",lw=4.0,label=L"$d_s\langle J\rangle$ from S3")
 
 
 
